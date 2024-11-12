bb10fd1765e6a6c75783de29f5734e3ccfbc8b93
Oskar <osk.kierys@gmail.com>
2024-11-12 14:33:14 +0100

    header file finished

--- File.hpp
+++ File.hpp
@@ -1,4 +1,26 @@
 #ifndef FILE_HPP
 #define FILE_HPP
 
+#include <string>
+#include <iostream>
+#include <vector>
+
+class File
+{
+private:
+    std::string m_filePath;
+
+public:
+    File(const std::string& path);
+
+    bool createFile();
+    bool writeFile(const std::string& content);
+    bool deleteFile();
+
+    std::string readFile();
+    static std::vector<std::string> listFilesInDirectory(const std::string& directoryPath);
+
+    void openInEditor();
+};
+
 #endif
